9958bf64525fd0f7a8ad051017fb718d3ef463c8
SCPomie <120145133+SCPomie@users.noreply.github.com>
2026-09-17 23:57:05 +0100

    Time: 55 ms (90.38%), Space: 19.7 MB (38.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,37 +1,33 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
+        if t == "":
             return ""
-        
-        count_t = {}
+
+        countT, window = {}, {}
         for c in t:
-            count_t[c] = count_t.get(c,0) + 1
-        required = len(count_t)
-        best_window = float("inf")
-        best_start = 0
-        formed = 0
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, need = 0, len(countT)
 
-        count_s = {}
+        res, resLen = [-1, -1], float("infinity")
+
         l = 0
-        for i, r in enumerate(s):
-            count_s[r] = count_s.get(r, 0) + 1
 
-            if r in count_t and count_t[r] == count_s[r]:
-                formed += 1
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
             
-            while formed == required:
-                curr_window = i - l + 1
-
-                if curr_window < best_window:
-                    best_window = curr_window
-                    best_start = l
-
-                left_char = s[l]
-                count_s[left_char] -= 1
-
-                if left_char in count_t and count_s[left_char] < count_t[left_char]:
-                    formed -= 1
-
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
                 l += 1
-        return "" if best_window == float("inf") else s[best_start: best_start + best_window]
-
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
