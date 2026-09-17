bb2330a5b47cb3cb2b25d8efb202b9d156062f50
SCPomie <120145133+SCPomie@users.noreply.github.com>
2026-09-17 19:36:33 +0100

    Time: 207 ms (52.35%), Space: 20.1 MB (9.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,11 +4,14 @@ class Solution:
         l = 0 
         strings = set()
         ans = 0
-
+        #goes over the list
         for r in range(len(s)):
+            # a while loop that checks for duplication
             while s[r] in strings:
+                #removes the leftmost element until the duplicate element is removed 
                 strings.remove(s[l])
                 l += 1
+            # adds the character into the list and calculate the current max length
             strings.add(s[r])
             ans = max(ans, len(strings))
         
