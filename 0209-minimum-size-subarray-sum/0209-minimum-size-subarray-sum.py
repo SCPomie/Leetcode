af96ad3a2ba93af8c77eb4b065ef228af4df41c7
SCPomie <120145133+SCPomie@users.noreply.github.com>
2026-09-17 19:18:52 +0100

    Time: 19 ms (44.95%), Space: 30.7 MB (16.95%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -3,15 +3,17 @@ class Solution:
         l = 0
         total = 0
         res = float("inf")
-
+        #starts at the the first index
         for r in range(len(nums)):
+            #adds up the numbers per index
             total += nums[r]
-
+            # when total >= target
             while total >= target:
+                # calculate the length, take away the total and increment l
                 res = min(res, r - l + 1)
                 total -= nums[l]
                 l += 1
-            
+        # return the result 
         return res if res != float("inf") else 0
 
              
