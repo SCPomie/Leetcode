fe9f787866901cd530d11020d9bdf6c3821a6267
SCPomie <120145133+SCPomie@users.noreply.github.com>
2026-09-21 22:18:51 +0100

    Time: 43 ms (76.98%), Space: 39.3 MB (27.94%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -4,18 +4,9 @@ class Solution:
         score = {}
 
         for i in range(len(nums)):
-            if nums[i] not in score:
-                score[nums[i]] = []
-            
-            score[nums[i]].append(i)
-            
-            if len(score[nums[i]]) >= 2:
-                n = score[nums[i]]
-                l, r = 0, len(n) - 1
-                while l < r:
-                    if abs(n[l] - n[r]) <= k:
-                        return True
-                    else:
-                        l += 1
+            if nums[i] in score and abs(i - score[nums[i]]) <= k:
+                return True
+            score[nums[i]] = i
         
         return False
+    
