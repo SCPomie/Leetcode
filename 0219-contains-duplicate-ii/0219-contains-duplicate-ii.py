e17209b227dbc14ef8afb42612f90bea09085e90
SCPomie <120145133+SCPomie@users.noreply.github.com>
2026-09-21 22:20:11 +0100

    Time: 43 ms (76.98%), Space: 39.3 MB (47.98%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,11 +1,14 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        
+        #initialise the dictionary
         score = {}
-
+        #loops over the list
         for i in range(len(nums)):
+            #if the current value is inside the dictionary, compared it with the last appearance of it
+            # in the dictionary. 
             if nums[i] in score and abs(i - score[nums[i]]) <= k:
                 return True
+            # adds only the most recent index of the value into the dictionary
             score[nums[i]] = i
         
         return False
