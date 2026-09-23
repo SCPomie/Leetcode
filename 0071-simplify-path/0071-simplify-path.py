6a5ece18f8c502860563e4d25b027f9a748c5d38
SCPomie <120145133+SCPomie@users.noreply.github.com>
2026-09-23 19:06:21 +0100

    Time: 4 ms (14.87%), Space: 19.3 MB (66.11%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -3,18 +3,23 @@ class Solution:
         
         #initialise a stack
         stack = []
+        #split the paths
         path = path.split('/')
-
+        #for characters in path
         for n in path:
+            # if its currently empty or just a single dot, just ignore and carry to next iteration
             if n == '' or n == '.':
                 continue
+            # if it is .. then pop the stack else continue to next if stack is empty
             elif n == '..':
                 if stack:
                     stack.pop()
                 else:
                     continue
+            #append the stack
             else:
                 stack.append(n)
-        
+        #joins the stack to get the final answer
         ans = ('/').join(stack)
+        #return / + ans if stack exists otherwise just return a single /
         return "/"+ans if stack else "/"
