ca14cd10abdd039fed6e3eb75cff70f98c41c070
SCPomie <120145133+SCPomie@users.noreply.github.com>
2026-09-24 02:25:14 +0100

    Time: 0 ms (100%), Space: 19.3 MB (92.37%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -7,17 +7,17 @@ class Solution:
     def reverseBetween(self, head: ListNode | None, left: int, right: int) -> ListNode | None:
         dummy = ListNode(0, head)
         leftPrev, cur = dummy, head
-
+        #traverse the linked list until left
         for _ in range(left - 1):
             leftPrev, cur = cur, cur.next
         
+        #reverse the linked list
         prev = None
-
         for _ in range(right - left + 1):
             tmpNext = cur.next
             cur.next = prev
             prev, cur = cur, tmpNext
-        
+        #reconnect the pointers using cur and prev
         leftPrev.next.next = cur
         leftPrev.next = prev
 
